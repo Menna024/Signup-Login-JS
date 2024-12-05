8dbf6437cfd6f66d55ca8fef0b2d492f82ffb81c
Menna024 <mennaayman0246@gmail.com>
2024-12-05 22:59:28 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,7 +1,63 @@
 var loginBtn=document.getElementById('loginBtn');
 
-loginBtn.addEventListener('click',function(){
+var email=document.getElementById('email');
+var password=document.getElementById('password');
 
+loginBtn.addEventListener('click',function(){
+    var isFullData=CheckVal(email.value,password.value);
+    console.log('isFullData '+isFullData);
+    if(isFullData)
+    {
+        var isCredValid=ValidateCredentials(email.value,password.value);
+        if(isCredValid)
+        {
+            alert('Login successful');
+            window.location.href='home.html';
+        }
+        else
+        {
+            alert('Invalid credentials');
+        }
+    }
+    else
+    {
+        alert('Please fill all fields');
+    }
 });
 
-var signUp=document.getElementById('signUp');
+
+function CheckVal(email,password)
+{
+    if(email=='' || password=='')
+    {
+        return false;
+    }
+    else
+        return true;
+}
+
+function ValidateCredentials(email,password)
+{
+    console.log('Validating credentials');
+    var userslist=JSON.parse(localStorage.getItem('userlist'));
+    var isUserFound=false;
+    for(var i=0;i<userslist.length;i++)
+    {
+        if(userslist[i].email==email && userslist[i].password==password)
+        {
+            isUserFound=true;
+            console.log('User found');
+            break;
+        }
+    }
+
+    if(isUserFound)
+    {        
+        console.log('GGG User found');
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
